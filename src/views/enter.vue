309dcd199b94fd19cedb37dafdce73c0c59ef00b
yufeng.zhang1@pactera.com <yufeng.zhang1@pactera.com>
2018-01-07 13:59:14 +0800

    English 7-25数据

--- src/views/enter.vue
+++ src/views/enter.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <ul class="catalog">
-      <router-link to="/math" tag="li">数学</router-link>
+      <router-link to="/math" tag="li">高等数学</router-link>
       <router-link to="/English" tag="li">英语</router-link>
       <router-link to="/computer" tag="li">计算机</router-link>
     </ul>
